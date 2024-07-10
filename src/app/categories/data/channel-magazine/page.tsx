3d244ef61fac4f628a993254b579319cf74908b6
Ehsan_Heidari <ehsan.heydari@gmail.com>
2024-07-09 23:36:38 -0700

    change subcategories and categories and organize overall

--- src/app/categories/data/channel-magazine/page.tsx
+++ src/app/categories/data/channel-magazine/page.tsx
@@ -61,7 +61,11 @@ const ChannelMagazine: React.FC = () => {
         // Fetch the lengths of each JSON file when the page loads
         const fetchJSONLengths = async () => {
             const lengths: JSONLengths = {}
-            const jsonFiles = ['Channel.json', 'Magazine.json'] // Replace with your actual JSON file names
+            const jsonFiles = [
+                'Channel.json',
+                'Magazine.json',
+                'Event Channel.json',
+            ] // Replace with your actual JSON file names
 
             for (const file of jsonFiles) {
                 try {
